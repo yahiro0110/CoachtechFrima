a3db56eedc5385ae3b759a2ce536949636c55c81
Hiroshi Yamashita <yama01hiro@gmail.com>
2024-02-08 21:39:54 +0900

    :zap: ドラッグ&ドロップ対応 画像選択時にドラッグ&ドロップでファイルを選択できるようにした。

--- src/resources/js/Pages/Items/Partials/StoreItemImagesForm.vue
+++ src/resources/js/Pages/Items/Partials/StoreItemImagesForm.vue
@@ -16,11 +16,6 @@ const props = defineProps({
 });
 
 /**
- * @requires defineEmits - カスタムイベントを定義するための関数
- */
-const emit = defineEmits(['update:formFile', 'handleFilesUpdate']);
-
-/**
  * リアクティブプロパティの定義。
  *
  * @property {Array} itemImages - 商品画像のプレビューを表示するための配列
@@ -36,6 +31,26 @@ let selectedFiles = ref([]);
  */
 const handleFileChange = (event) => {
     const newFiles = Array.from(event.target.files);
+    itemImages.value = []; // リセット
+    selectedFiles.value = []; // リセット
+    newFiles.forEach((file) => {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            itemImages.value.push(e.target.result);
+            selectedFiles.value.push(file);
+        };
+        reader.readAsDataURL(file);
+    });
+};
+
+/**
+ * ドラッグ&ドロップ時に呼び出され、商品画像プレビューを更新する。
+ *
+ * @param {Event} event - ドラッグ&ドロップイベント
+ */
+const handleDrop = (event) => {
+    event.preventDefault();
+    const newFiles = Array.from(event.dataTransfer.files);
     itemImages.value = []; // リセット
     selectedFiles.value = []; // リセット
     newFiles.forEach((file) => {
@@ -127,7 +142,7 @@ const filesErrorMessage = computed(() => {
         </div>
 
         <div class="col-span-full">
-            <div class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+            <div class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10" @dragover.prevent="e => e.dataTransfer.dropEffect = 'copy'" @drop="handleDrop">
                 <div class="text-center">
                     <svg class="mx-auto h-12 w-12 text-gray-300" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
                         <path fill-rule="evenodd" d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z" clip-rule="evenodd" />
