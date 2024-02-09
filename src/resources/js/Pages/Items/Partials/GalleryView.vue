befd268ad67374d360047d42026d155c1a69cb55
Hiroshi Yamashita <yama01hiro@gmail.com>
2024-02-09 18:52:16 +0900

    :lipstick: 商品画像表示ビューの見直し 商品画像プレビューで何枚目/総数を表示するようにした。

--- src/resources/js/Pages/Items/Partials/GalleryView.vue
+++ src/resources/js/Pages/Items/Partials/GalleryView.vue
@@ -99,6 +99,9 @@ const showPreviousImage = () => {
                     </svg>
                     <span class="text-sm">前の画像</span>
                 </a>
+                <div class="text-light">
+                    {{ currentIndex + 1 }} / {{ itemImages.length }}
+                </div>
                 <a class="text-light flex items-center cursor-pointer" @click="showNextImage">
                     <span class="text-sm">次の画像</span>
                     <svg class="flex-shrink-0 w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
