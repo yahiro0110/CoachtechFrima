1edb2573f1e7fa223675a7a6efca12543a0622a9
Hiroshi Yamashita <yama01hiro@gmail.com>
2024-02-19 20:55:32 +0900

    :lipstick: ユーザへの歓迎メッセージを追加

--- src/resources/js/Layouts/GuestLayout.vue
+++ src/resources/js/Layouts/GuestLayout.vue
@@ -5,10 +5,11 @@ import { Link } from '@inertiajs/inertia-vue3';
 
 <template>
     <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-        <div>
+        <div class="flex flex-col items-center">
             <Link href="/">
             <ApplicationLogo class="w-20 h-20 fill-current text-gray-500" />
             </Link>
+            <p class="font-great-vibes text-gray-500 text-center text-xl mt-2">Welcome to a journey of rediscovery!<br />Log in and check out today's treasures.</p>
         </div>
 
         <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
