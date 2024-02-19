c5a22a6dfab5f4b78229da51429855d2bab93d89
Hiroshi Yamashita <yama01hiro@gmail.com>
2024-02-19 18:31:14 +0900

    :card_file_box: categoriesテーブル用のシーダーファイルでサンプルの内容を男性向けに見直した。

--- src/database/seeders/CategorySeeder.php
+++ src/database/seeders/CategorySeeder.php
@@ -14,284 +14,102 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        // 初期データの定義
-        $initialCategories = [
-            [
-                'name' => '服',
-            ],
-            [
-                'name' => '電化製品',
-            ],
-            [
-                'name' => '家具',
-            ],
-            [
-                'name' => '本',
-            ],
-            [
-                'name' => 'その他',
-            ],
-            [
-                'name' => 'メンズ',
-                'parent_id' => 1,
-            ],
-            [
-                'name' => 'レディース',
-                'parent_id' => 1,
-            ],
-            [
-                'name' => 'キッズ',
-                'parent_id' => 1,
-            ],
-            [
-                'name' => 'テレビ',
-                'parent_id' => 2,
-            ],
-            [
-                'name' => 'エアコン',
-                'parent_id' => 2,
-            ],
-            [
-                'name' => '洗濯機',
-                'parent_id' => 2,
-            ],
-            [
-                'name' => '冷蔵庫',
-                'parent_id' => 2,
-            ],
-            [
-                'name' => 'ソファ',
-                'parent_id' => 3,
-            ],
-            [
-                'name' => 'ベッド',
-                'parent_id' => 3,
-            ],
-            [
-                'name' => '机',
-                'parent_id' => 3,
-            ],
-            [
-                'name' => '椅子',
-                'parent_id' => 3,
-            ],
-            [
-                'name' => '小説',
-                'parent_id' => 4,
-            ],
-            [
-                'name' => '漫画',
-                'parent_id' => 4,
-            ],
-            [
-                'name' => '専門書',
-                'parent_id' => 4,
-            ],
-            [
-                'name' => 'スポーツ用品',
-                'parent_id' => 5,
-            ],
-            [
-                'name' => '楽器',
-                'parent_id' => 5,
-            ],
-            [
-                'name' => 'ジャケット',
-                'parent_id' => 6,
-            ],
-            [
-                'name' => 'シャツ',
-                'parent_id' => 6,
-            ],
-            [
-                'name' => 'ワンピース',
-                'parent_id' => 7,
-            ],
-            [
-                'name' => 'スカート',
-                'parent_id' => 7,
-            ],
-            [
-                'name' => 'トップス',
-                'parent_id' => 7,
-            ],
-            [
-                'name' => '体操着',
-                'parent_id' => 8,
-            ],
-            [
-                'name' => '液晶モニター',
-                'parent_id' => 9,
-            ],
-            [
-                'name' => 'ワンセグ',
-                'parent_id' => 9,
-            ],
-            [
-                'name' => '室内用',
-                'parent_id' => 10,
-            ],
-            [
-                'name' => 'ポータブル',
-                'parent_id' => 10,
-            ],
-            [
-                'name' => 'ドラム式',
-                'parent_id' => 11,
-            ],
-            [
-                'name' => '縦型',
-                'parent_id' => 11,
-            ],
-            [
-                'name' => '二階層式',
-                'parent_id' => 11,
-            ],
-            [
-                'name' => '小型タイプ',
-                'parent_id' => 12,
-            ],
-            [
-                'name' => '大型タイプ',
-                'parent_id' => 12,
-            ],
-            [
-                'name' => 'シングル',
-                'parent_id' => 13,
-            ],
-            [
-                'name' => '二人掛け',
-                'parent_id' => 13,
-            ],
-            [
-                'name' => '三人掛け',
-                'parent_id' => 13,
-            ],
-            [
-                'name' => 'シングル',
-                'parent_id' => 14,
-            ],
-            [
-                'name' => 'ダブル',
-                'parent_id' => 14,
-            ],
-            [
-                'name' => '〜90cm',
-                'parent_id' => 15,
-            ],
-            [
-                'name' => '90〜120cm',
-                'parent_id' => 15,
-            ],
-            [
-                'name' => '120〜180cm',
-                'parent_id' => 15,
-            ],
-            [
-                'name' => 'ダイニングチェア',
-                'parent_id' => 16,
-            ],
-            [
-                'name' => 'アームチェア',
-                'parent_id' => 16,
-            ],
-            [
-                'name' => 'オフィスチェア',
-                'parent_id' => 16,
-            ],
-            [
-                'name' => '芥川龍之介',
-                'parent_id' => 17,
-            ],
-            [
-                'name' => '太宰治',
-                'parent_id' => 17,
-            ],
-            [
-                'name' => '夏目漱石',
-                'parent_id' => 17,
-            ],
-            [
-                'name' => 'ジョジョの奇妙な冒険',
-                'parent_id' => 18,
-            ],
-            [
-                'name' => 'はじめの一歩',
-                'parent_id' => 18,
-            ],
-            [
-                'name' => '賭博黙示録カイジ',
-                'parent_id' => 18,
-            ],
-            [
-                'name' => 'NARUTO -ナルト-',
-                'parent_id' => 18,
-            ],
-            [
-                'name' => '数学',
-                'parent_id' => 19,
-            ],
-            [
-                'name' => '歴史',
-                'parent_id' => 19,
-            ],
-            [
-                'name' => '経済',
-                'parent_id' => 19,
-            ],
-            [
-                'name' => 'サッカー',
-                'parent_id' => 20,
-            ],
-            [
-                'name' => 'バスケットボール',
-                'parent_id' => 20,
-            ],
-            [
-                'name' => '野球',
-                'parent_id' => 20,
-            ],
-            [
-                'name' => 'テニス',
-                'parent_id' => 20,
-            ],
-            [
-                'name' => 'ギター',
-                'parent_id' => 21,
-            ],
-            [
-                'name' => 'ベース',
-                'parent_id' => 21,
-            ],
-            [
-                'name' => 'ピアノ',
-                'parent_id' => 21,
-            ],
-            [
-                'name' => 'ドラム',
-                'parent_id' => 21,
-            ],
-            [
-                'name' => 'バット',
-                'parent_id' => 60,
-            ],
-            [
-                'name' => 'グローブ',
-                'parent_id' => 60,
-            ],
-            [
-                'name' => 'ボール',
-                'parent_id' => 60,
-            ],
-            [
-                'name' => 'ラケット',
-                'parent_id' => 61,
-            ],
-        ];
+        // メインカテゴリーの作成
+        $fashionId = Category::create(['name' => 'ファッション'])->id;
+        $gadgetId = Category::create(['name' => 'ガジェット'])->id;
+        $outdoorId = Category::create(['name' => 'アウトドア'])->id;
+        $sportsId = Category::create(['name' => 'スポーツ'])->id;
+        $hobbyId = Category::create(['name' => '趣味'])->id;
 
-        // 各初期データを挿入する
-        foreach ($initialCategories as $categoryData) {
-            Category::create($categoryData);
-        }
+        // ファッションのサブカテゴリー
+        $jacketId = Category::create(['name' => 'ジャケット', 'parent_id' => $fashionId])->id;
+        Category::create(['name' => 'ミリタリージャケット', 'parent_id' => $jacketId]);
+        Category::create(['name' => 'フライトジャケット', 'parent_id' => $jacketId]);
+        Category::create(['name' => 'ダウンジャケット', 'parent_id' => $jacketId]);
+
+        $shirtId = Category::create(['name' => 'シャツ', 'parent_id' => $fashionId])->id;
+        Category::create(['name' => 'Tシャツ', 'parent_id' => $shirtId]);
+        Category::create(['name' => 'ワイシャツ', 'parent_id' => $shirtId]);
+
+        $pantsId = Category::create(['name' => 'パンツ', 'parent_id' => $fashionId])->id;
+        Category::create(['name' => 'ジーンズ', 'parent_id' => $pantsId]);
+        Category::create(['name' => 'スラックス', 'parent_id' => $pantsId]);
+        Category::create(['name' => 'チノパン', 'parent_id' => $pantsId]);
+
+        $shoesId = Category::create(['name' => '靴', 'parent_id' => $fashionId])->id;
+        Category::create(['name' => 'フォーマルシューズ', 'parent_id' => $shoesId]);
+        Category::create(['name' => 'カジュアルシューズ', 'parent_id' => $shoesId]);
+        Category::create(['name' => 'ブーツ', 'parent_id' => $shoesId]);
+        Category::create(['name' => 'サンダル', 'parent_id' => $shoesId]);
+
+        // ガジェットのサブカテゴリー
+        $smartphoneId = Category::create(['name' => 'スマートフォン', 'parent_id' => $gadgetId])->id;
+        Category::create(['name' => 'Android', 'parent_id' => $smartphoneId]);
+        Category::create(['name' => 'Apple', 'parent_id' => $smartphoneId]);
+
+        $pcId = Category::create(['name' => 'パソコン', 'parent_id' => $gadgetId])->id;
+        $desktopId = Category::create(['name' => 'デスクトップ', 'parent_id' => $pcId])->id;
+        Category::create(['name' => 'タワーケース', 'parent_id' => $desktopId]);
+        Category::create(['name' => 'ミッドタワーケース', 'parent_id' => $desktopId]);
+        Category::create(['name' => 'ミニタワーケース', 'parent_id' => $desktopId]);
+
+        $notebookId = Category::create(['name' => 'ノートブック', 'parent_id' => $pcId])->id;
+        Category::create(['name' => 'クラムシェル ノートパソコン', 'parent_id' => $notebookId]);
+        Category::create(['name' => '2 in 1 ノートパソコン', 'parent_id' => $notebookId]);
+
+        // アウトドアのサブカテゴリー
+        $campId = Category::create(['name' => 'キャンプ', 'parent_id' => $outdoorId])->id;
+        Category::create(['name' => 'テント', 'parent_id' => $campId]);
+        Category::create(['name' => 'ランダン', 'parent_id' => $campId]);
+        Category::create(['name' => 'アウトドアウェア', 'parent_id' => $campId]);
+
+        $hikingId = Category::create(['name' => 'ハイキング', 'parent_id' => $outdoorId])->id;
+        Category::create(['name' => 'バックパック', 'parent_id' => $hikingId]);
+        Category::create(['name' => 'トレッキングポール', 'parent_id' => $hikingId]);
+
+        $fishingId = Category::create(['name' => '釣り', 'parent_id' => $outdoorId])->id;
+        Category::create(['name' => '竿', 'parent_id' => $fishingId]);
+        Category::create(['name' => 'リール', 'parent_id' => $fishingId]);
+        Category::create(['name' => 'クーラボックス', 'parent_id' => $fishingId]);
+        $lureId = Category::create(['name' => 'ルアー', 'parent_id' => $fishingId])->id;
+        Category::create(['name' => 'ミノー', 'parent_id' => $lureId]);
+        Category::create(['name' => 'クランクベイト', 'parent_id' => $lureId]);
+        Category::create(['name' => 'ワーム', 'parent_id' => $lureId]);
+
+        // スポーツのサブカテゴリー
+        $soccerId = Category::create(['name' => 'サッカー', 'parent_id' => $sportsId])->id;
+        $futsalId = Category::create(['name' => 'フットサル', 'parent_id' => $soccerId])->id;
+        Category::create(['name' => 'サッカーボール', 'parent_id' => $futsalId]);
+        Category::create(['name' => 'シューズ', 'parent_id' => $futsalId]);
+        // フットサルとフットボールでカテゴリーは同じなので、繰り返さずに示します
+
+        $basketballId = Category::create(['name' => 'バスケット', 'parent_id' => $sportsId])->id;
+        Category::create(['name' => 'バスケットボール', 'parent_id' => $basketballId]);
+
+        $badmintonId = Category::create(['name' => 'バドミントン', 'parent_id' => $sportsId])->id;
+        Category::create(['name' => 'ラケット', 'parent_id' => $badmintonId]);
+        Category::create(['name' => 'シャトル', 'parent_id' => $badmintonId]);
+
+        // 趣味のサブカテゴリー
+        $modelId = Category::create(['name' => '模型', 'parent_id' => $hobbyId])->id;
+        $plasticModelId = Category::create(['name' => 'プラスチックモデル', 'parent_id' => $modelId])->id;
+        Category::create(['name' => '飛行機', 'parent_id' => $plasticModelId]);
+        Category::create(['name' => '車', 'parent_id' => $plasticModelId]);
+        Category::create(['name' => '船', 'parent_id' => $plasticModelId]);
+        Category::create(['name' => '軍用車両', 'parent_id' => $plasticModelId]);
+
+        $woodModelId = Category::create(['name' => '木製モデル', 'parent_id' => $modelId])->id;
+        Category::create(['name' => '船', 'parent_id' => $woodModelId]);
+        Category::create(['name' => '車', 'parent_id' => $woodModelId]);
+        Category::create(['name' => '飛行機', 'parent_id' => $woodModelId]);
+
+        $metalModelId = Category::create(['name' => '金属製モデル', 'parent_id' => $modelId])->id;
+        Category::create(['name' => '車', 'parent_id' => $metalModelId]);
+        Category::create(['name' => 'トラック', 'parent_id' => $metalModelId]);
+        Category::create(['name' => '飛行機', 'parent_id' => $metalModelId]);
+
+        $gameId = Category::create(['name' => 'ゲーム', 'parent_id' => $hobbyId])->id;
+        Category::create(['name' => 'カード', 'parent_id' => $gameId]);
+        Category::create(['name' => 'レトロゲーム', 'parent_id' => $gameId]);
     }
 }
